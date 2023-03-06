7dc178da59f1726aa499d57c65b4c16950748623
faiqalyv <faigra@code.edu.az>
2023-03-06 10:26:05 +0400

    first commit

---
+++ CustomJoin/Program.cs
@@ -0,0 +1,25 @@
+﻿tring CustomJoin(string[] arr, string sep)
+{
+    string a = "";
+    for (int i = 0; i < arr.Length; i++)
+    {
+
+
+        if (i == arr.Length - 1)
+        {
+            a += arr[i];
+
+        }
+        else
+        {
+            a += arr[i] + sep;
+        }
+
+
+
+    }
+    return a;
+
+}
+Console.WriteLine(CustomJoin(new string[] { "1", "2", "3" }, "-"));
+
